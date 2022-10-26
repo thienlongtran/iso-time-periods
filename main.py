f01c79cb0153f2b47511a0fdbe181852e3510600
thienlongtran <ttran384@gatech.edu>
2022-10-25 23:07:18 -0500

    Generate and print interval start and end times

---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime
+
+current_time = datetime.datetime.utcnow()
+
+interval_end_time = current_time - datetime.timedelta(
+    minutes=current_time.minute % 5,
+    seconds=current_time.second,
+    microseconds=current_time.microsecond)
+
+interval_start_time = interval_end_time - datetime.timedelta(minutes=5)
+
+print("Start Interval Time: " + interval_start_time.isoformat())
+print("End Interval Time: " + interval_end_time.isoformat())
